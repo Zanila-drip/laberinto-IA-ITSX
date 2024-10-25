c92ba0c9f5316d1c0be21f4f98b67aa5d9cf2446
Zanila-drip <mleo0499@gmail.com>
2024-10-25 01:10:41 -0600

    Vamos a implementar las pruebas en el original!

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,56 @@
 import 'dart:io';
 import 'package:excel/excel.dart';
 
-void main()
-{
-    //Dicen que el codigo es como los chistes
-    //Si lo explicas, pierde su gracia!
-    //Enjoy! Zanila-Drip spookyTime-2024
-    List<List<int>> matrix = [[],[]];
-    var file = 'archivosExcel/pruebaUno.xlsx ';
+void main() {
+    /*
+        They say code is like jokes
+        If you have to explain it, it loses its charm!
+        Enjoy! Zanila-Drip spookyTime-2024
+     */
+    List<List<int>> matrix = [];
+    var file = 'archivosExcel/pruebaErrorUno.xlsx ';
     var bytes = File(file).readAsBytesSync();
     var excel = Excel.decodeBytes(bytes);
 
     for (var table in excel.tables.keys)
     {
-        for (var row in excel.tables[table]!.rows)
+        /*
+            Siempre es bueno conocer el numero de columnas y filas!
+            print(excel.tables[table].maxColumns);
+            print(excel.tables[table].maxRows);
+         */
+        if (excel.tables[table]?.maxColumns == excel.tables[table]?.maxRows)
         {
-            for (var cell in row)
+            print("Si bro son iguales!");
+            for (var row in excel.tables[table]!.rows)
             {
-                final value = cell?.value;
-                print('Celda: ${cell?.rowIndex}/${cell?.columnIndex} valor: $value');
-                /*
-                    Codigo extenso que hice para convertir el tipo de dato CellValue en un entero para poder meterlo a la matriz
-                    Solo dios entiende las cosas, por que Dart las complica tanto?
-                    Pero bueno aqui andamos!
-                 */
-                var valor = value.toString();
-                print(valor);
-                int valorInt = int.parse(valor);
-                int productoValor = valorInt + 2;
-                print(productoValor);
+                for (var cell in row)
+                {
+                    final value = cell?.value;
+                    print('Cell: ${cell?.rowIndex}/${cell?.columnIndex} value: $value');
+                    /*
+                    Extensive code I wrote to convert the CellValue data type into an integer so
+                    I can insert it into the matrix
+                    Only God understands these things.
+                    Why does Dart make everything so complicated?
+                    But here we are!
+                */
+                    var valor = value.toString();
+                    print(valor);
+                    int valorInt = int.parse(valor);
+                }
             }
         }
+
+        else {
+            print("No bro, no son iguales. No sirvo lo siento :(");
+        }
+        /*
+            Es bueno conocer el numero de filas y columnas!
+            print(excel.tables[table].maxColumns);
+            print(excel.tables[table].maxRows);
+        */
+
+
     }
-
 }
-
---
+++ lib/pruebaUno.dart
@@ -0,0 +1,39 @@
+import 'dart:math';
+
+import 'package:excel/excel.dart';
+void main()
+{
+  List<List<int>> matrix=[[]];
+  int columnas = 5-1;
+  int filas = columnas;
+  int contador = 0;
+  int numeroRandom = 0;
+  int valor = 4;
+  int inicio = 0;
+
+  while(contador <= filas)
+  {
+    print("Javier se fumo $contador porros");
+    matrix.add([]);
+    for (int i=0; i<=filas;i++)
+      {
+        numeroRandom = Random().nextInt(10);
+        matrix[contador].add(numeroRandom);
+      }
+
+    contador++;
+  }
+  print(matrix);
+
+/*
+  print(matrix);
+  inicio = inicio + 1;
+  valor = valor + 1;
+  matrix.insert(inicio,[valor]);
+  print(matrix);
+  valor = valor++;
+  matrix[inicio].add(valor);
+  print(matrix);
+ */
+
+}
